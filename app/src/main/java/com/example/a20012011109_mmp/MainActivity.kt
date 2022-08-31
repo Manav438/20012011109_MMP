3dff68ef39dc4c6c53feb8409fd61bebeb5a0b26
Manav Patel <manavkumarpatel20@gnu.ac.in>
2022-08-31 21:53:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a20012011109_mmp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.a20012011109_mmp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    val tag="MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    private fun showmessage(msg:String){
+        Log.i(tag,msg)
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showmessage("Congratulations ! You Achieved ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showmessage("Great")
+    }
+}
